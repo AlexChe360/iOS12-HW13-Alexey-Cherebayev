5d6c2831ffce7e521caabe75afc140114e26f5f1
AlexChe360 <154796294+AlexChe360@users.noreply.github.com>
2024-02-01 11:28:07 +0600

    Update iOS12-HW13-Alexey Cherebayev/Sources/Controllers/ViewController.swift
    Fixed
    
    Co-authored-by: LtFars <89894467+LtFars@users.noreply.github.com>

--- iOS12-HW13-Alexey Cherebayev/Sources/Controllers/ViewController.swift
+++ iOS12-HW13-Alexey Cherebayev/Sources/Controllers/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
     
     // MARK: - UI
     lazy var tableView: UITableView = {
-        let table = UITableView(frame: .zero, style: .grouped)
+        let table = UITableView(frame: .zero, style: .insetGrouped) // ну у меня на айфоне во всяком случае так
         table.register(SettingTableViewCell.self, forCellReuseIdentifier: SettingTableViewCell.identifier)
         table.register(SwitchTableViewCell.self, forCellReuseIdentifier: SwitchTableViewCell.identifier)
         return table
